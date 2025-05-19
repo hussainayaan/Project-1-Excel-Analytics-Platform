ebb76fe9fe65397246d29b1e77aa67723081f1b4
Hussain <67498029+hussainayaan@users.noreply.github.com>
2025-05-19 14:22:40 +0530

    Create Routes.js

---
+++ backend/ backend/Routes.js
@@ -0,0 +1,16 @@
+// backend/routes/uploadRoutes.js
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { handleUpload } = require("../controllers/uploadController");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("file"), handleUpload);
+
+module.exports = router;
