f123773ea5cb531dbaf0797d9b994826e5100b5e
Hussain <67498029+hussainayaan@users.noreply.github.com>
2025-05-19 14:25:28 +0530

    Create App.js

---
+++ frontend/src/components/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FileUpload from "./components/FileUpload";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>📈 Excel Analytics Platform</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
